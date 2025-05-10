26ba5c780dfaabcab0a2a89dbfdc0a7abf012f63
Anshika1378 <anshikaagarwal56890@gmail.com>
2025-05-10 11:42:28 +0530

    chat-commit

---
+++ chatai.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded',function(){
+    const botai = document.getElementById('botai');
+    const userinput = document.getElementById('userinput');
+    const sendBtn = document.getElementById('sendBtn');
+
+    const responses = {
+        hello :'hi, how can i assist you?',
+        'how are you.': 'I am a robot but i am fine what about you?',
+        'Whats your name': 'i am AI',
+        'can you solve a one things': 'Yeah why not! how can i help you?',
+        'can i ask a one question': 'Yeah why not! how can i help you?',
+        'solve this question 2 + 2': 'Its a mathematics question and addition. Here the answer is 4',
+        'thanks':'Most Welcome',
+        Bye: ' Bye ! have a great day',
+        default: "I'm not sure about that. Can you ask something else?",
+    };
+
+    function appendMessage(text, className){
+        const messagediv = document.createElement('div');
+        messagediv.classList.add(className);
+        messagediv.textContent = text;
+        botai.appendChild(messagediv);
+        botai.scrollTop = botai.scrollHeight;
+    }
+
+    function botreply(userText){
+        const lowercaseText =  userText.toLowerCase();
+        return responses[lowercaseText] || responses['default'];
+    }   
+
+    sendBtn.addEventListener('click',function(){
+        const userText = userinput.value.trim();
+        if(userText === '') return ;
+
+        appendMessage(userText,'usermessage');
+        setTimeout(()=>{
+            const botresponse = botreply(userText);
+            appendMessage(botresponse,'botmessage')
+        },500)
+        userinput.value = '';
+    });
+
+    userinput.addEventListener('keypress',function(event){
+        if(event.key === 'Enter'){
+            sendBtn.click();
+        }
+    });
+
+});
+
